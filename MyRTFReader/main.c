d5aff46e536dc6b5b4db89a8980f55d20286efb2
Kjell-Olov Högdahl <kjell-olov.hogdahl@itfied.se>
2022-06-02 15:18:21 +0200

    Initial Commit

---
+++ MyRTFReader/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MyRTFReader
+//
+//  Created by Kjell-Olov Högdal on 2022-06-02.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
